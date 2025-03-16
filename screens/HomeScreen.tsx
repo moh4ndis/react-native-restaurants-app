d0fa9ef659d1cd21aac9421a98de2f97c65aa1ee
moh4ndis <elanaamimohamed@gmail.com>
2025-03-16 03:39:23 +0000

    HomeScreen

---
+++ screens/HomeScreen.tsx
@@ -0,0 +1,24 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { restaurants } from "../data/restaurant";
+import { useNavigation } from "@react-navigation/native";
+
+const HomeScreen = () => {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen </Text>
+    </View>
+  );
+};
+
+export default HomeScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
