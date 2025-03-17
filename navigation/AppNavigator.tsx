ba8e461216db97700435427271ff32e829b10f24
moh4ndis <elanaamimohamed@gmail.com>
2025-03-17 02:25:48 +0000

    restaurant component

--- navigation/AppNavigator.tsx
+++ navigation/AppNavigator.tsx
@@ -3,16 +3,35 @@ import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import HomeScreen from "../screens/HomeScreen";
-import RestaurantDetails from "../screens/RestaurantDetails";
+import RestaurantDetailsScreen from "../screens/RestaurantDetailsScreen";
 
-const Stack = createStackNavigator();
+type RootStackParamList = {
+  Home: undefined;
+  RestaurantDetails: {
+    restaurant: {
+      image: string;
+      name: string;
+      description: string;
+      menu: {
+        id: string;
+        name: string;
+        price: string;
+      }[];
+    };
+  };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="RestaurantDetails" component={RestaurantDetails} />
+        <Stack.Screen
+          name="RestaurantDetails"
+          component={RestaurantDetailsScreen}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
